80b3618644ec39d3f6a729e7966f9f94b4c0fde4
emil kodehode <emilas.kodehode@gmail.com>
2023-03-21 10:31:43 +0100

    started pokedex for realsies

--- JS/mypokedex.js
+++ JS/mypokedex.js
@@ -8,6 +8,33 @@ todo:
 make an utility fetch function?
 get some poke info
 display some pokeinfo
+
+-   -   -   -   -   -   -   -   -   -
+main goal to ahcieve is this:
+3 card state rest hover clicked.
+when card is hoverd over base info like hp type is shown expanded in a transition over the image
+-   -   -   -   -   -   -   -   -   -
+
+
+|   |   |   |   |   |   |   |   |   |
+big end goal is this one
+when clicked the card image moves left and hover is instead expanded in a transition to the right at the bottom showing stats
+right side of card is expanded and more info is shown like flavor text favorit berry and weight moves and such
+|   |   |   |   |   |   |   |   |   |
+
+
+so card container div
+name id
+image
+symbols
+
+on hover symbols expand into base stat information
+hover has container with list of info inside.
+
+i fear my pokemon card assembler function thing is gonna be a mess and very heavy
+
+
+
 */
 
 const pokeContainerEL = document.getElementById("pokemaincontainer")
@@ -22,23 +49,24 @@ async function apiGet(url){
 }
 
 async function pokemonTest(){
-    pokemonData = await apiGet("https://pokeapi.co/api/v2/pokemon?offset=0&limit=5")
+    pokemonData = await apiGet("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
     const pageMeat = pokemonData.results
     let arrayOfPokemon = []
     for (const meat of pageMeat) {
-        arrayOfPokemon.push(pageBuilder(meat))
+        arrayOfPokemon.push(await pokemonCardMaker(meat))
     }
     pokeContainerEL.append(...arrayOfPokemon)
 }
 
 pokemonTest()
 
-function pageBuilder(obj){
+async function pokemonCardMaker(obj){
     const {name, url} = obj
     const title = document.createElement("p")
     title.textContent = name
     const picture = document.createElement("img")
-    //picture.src = "url"
+    const singlePokeData = await apiGet(url)
+    picture.src = singlePokeData.sprites.other["official-artwork"].front_default
     picture.alt = name
     const card = document.createElement("div")
     card.className = "card"
